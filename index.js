51827d76028918ec70746d1f377b6b2897bab1ee
furixturi <alabebop@gmail.com>
2018-04-25 20:41:59 +0900

    cluster with 1 worker, 1 thread

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
+process.env.UV_THREADPOOL_SIZE = 1; // Use one thread for each cluster instance
 const cluster = require('cluster');
-console.log(cluster.isMaster); // if it is the cluster manager or a worker instance?
+const crypto = require('crypto');
 
 // Is the file being executed in master mode?
 if(cluster.isMaster){
     // Cause index.js to be executed again but in child mode
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
+    cluster.fork(); // One worker
 } else {
     // I am a child, I'm going to act like a normal express server and do nothing else
     const express = require('express');
     const app = express();
 
-    function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration) {}
-    }
-
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there');
+        crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+            res.send("Hi there");
+        });
     });
 
     app.get('/fast', (req, res) => {
