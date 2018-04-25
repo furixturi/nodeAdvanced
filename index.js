4ab5e864dc5c5e206785de65859aa833839f5c19
furixturi <alabebop@gmail.com>
2018-04-25 21:00:25 +0900

    Clean up to use pm2

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-process.env.UV_THREADPOOL_SIZE = 1; // Use one thread for each cluster instance
-const cluster = require('cluster');
 const crypto = require('crypto');
+const express = require('express');
+const app = express();
 
-// Is the file being executed in master mode?
-if(cluster.isMaster){
-    // Cause index.js to be executed again but in child mode
-    cluster.fork(); // First worker
-    cluster.fork(); // Second worker
-    cluster.fork(); // Second worker
-    cluster.fork(); // Second worker
-    // cluster.fork(); // Second worker
-    // cluster.fork(); // Second worker
-} else {
-    // I am a child, I'm going to act like a normal express server and do nothing else
-    const express = require('express');
-    const app = express();
-
-    app.get('/', (req, res) => {
-        crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-            res.send("Hi there");
-        });
+app.get('/', (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+        res.send("Hi there");
     });
+});
 
-    app.get('/fast', (req, res) => {
-       res.send('This was fast!');
-    })
-    app.listen(3000);
-}
+app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+})
+app.listen(3000);
+
